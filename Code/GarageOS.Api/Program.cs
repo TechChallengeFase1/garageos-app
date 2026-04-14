9c8d5c728b2bdac0d2dfe2b7e9a7f9479173b10c
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2026-04-13 21:17:48 -0300

    Add swagger na api

--- Code/GarageOS.Api/Program.cs
+++ Code/GarageOS.Api/Program.cs
@@ -1,10 +1,12 @@
 using GarageOS.Api.Extensions;
-using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "GarageOS API", Version = "v1" });
+});
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplicationServices();
@@ -13,8 +15,12 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "GarageOS API v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
